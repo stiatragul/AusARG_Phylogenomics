4f6761bcb2c69494b8d4eca00c1c2c09c8b4d1d8
ST <feathertailglidr@gmail.com>
2022-03-02 11:25:47 +1100

    init

---
+++ code/filter_rejects.R
@@ -0,0 +1,6 @@
+# filter_rejects.R
+# 2022-03-02
+
+# Following SqCL pipeline workflow step one deduplication and filter
+# We identified several samples that doesn't seem to 'work', that is there
+# were no outputs as R1_bb.final.fq.gz files. These are then supposed to be 
